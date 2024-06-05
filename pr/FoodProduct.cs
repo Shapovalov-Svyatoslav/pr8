1340c9c65e9489b3b872be3231c59318f3efa590
Shapovalov-Svyatoslav <rudic207r@gmail.com>
2024-06-05 22:21:26 +0300

    add project

---
+++ pr/FoodProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr
+{
+	public class FoodProduct : Product, IConsumable
+	{
+		public FoodProduct(string name, int price, DateTime expiryDate) : base(name, price)
+		{
+			this.expiryDate = expiryDate;
+		}
+
+        public DateTime expiryDate { get; set; }
+
+		public void Consume()
+		{
+            Console.WriteLine($"Product {name} is consumed");
+        }
+	}
+}
---
+++ pr/Program.cs
@@ -0,0 +1,11 @@
+﻿using pr;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var food = new FoodProduct("apple", 1, DateTime.Today.AddDays(8));
+
+		food.Consume();
+	}
+}
